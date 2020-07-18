e2b8bbed8722b5cc63d6556b06bf56b62f14a7b5
Tuan Hung <vutuanhung98@gmail.com>
2020-07-19 02:15:56 +0700

    Add setup file for installing dependencies

---
+++ setup.cmd
@@ -0,0 +1,10 @@
+@echo off
+pip install -e .
+
+echo.
+echo ########################################################
+echo # [FINISHED] All packages were successfully installed. #
+echo ########################################################
+echo.
+
+set /P var =Press Enter to exit.
