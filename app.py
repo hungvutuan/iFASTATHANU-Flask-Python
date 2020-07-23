81c3bfb2571c38d1be42300b8afedb27bff50c55
Tuan Hung <vutuanhung98@gmail.com>
2020-07-23 16:24:31 +0700

    Add routing queries and HTTP error handlers

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
+# coding: utf-8
 import os
 
-from flask import Flask, render_template, jsonify, request, send_from_directory
+from flask import Flask, render_template, jsonify, request, send_from_directory, abort
 import json
+
+from mysql.connector import DatabaseError
+
 from database import database as db
 from templates import decorator_serverboot
 
 app = Flask(__name__)
-app.secret_key = "vth"
+app.secret_key = "4,\x178sg\xde=U=\xa7\xe5Hr\x11\xaf"
 decorator_serverboot.decorate()
 
 
@@ -73,24 +77,17 @@ def error_conflict():
 
 @app.errorhandler(404)
 def error_not_found(error=None):
-    resp = jsonify({
-        'status': 404,
-        'message': 'Not found: ' + request.url,
-        'error': error
-    })
-    resp.status_code = 404
-    return resp
+    return render_template("error_404.html"), 404
+
+
+@app.errorhandler(405)
+def error_method_not_allowed(error=None):
+    return render_template("error_405.html"), 405
 
 
 @app.errorhandler(500)
 def error_internal_server_error(error=None):
-    resp = jsonify({
-        "status": 500,
-        "message": "Internal server error at " + request.url,
-        "error": error
-    })
-    resp.status_code = 500
-    return resp
+    return render_template("error_500.html"), 500
 
 
 @app.route('/metrics', methods=['GET', 'POST'])
@@ -103,6 +100,83 @@ def get_metrics():
 def history():
     if request.method == 'GET':
         return db.get_all_history_sensor()
+
+
+@app.route('/device/new', methods=['POST'])
+def insert_device():
+    if request.method == 'POST':
+        try:
+            device_name = request.args.get("deviceName")
+            device_type = request.args.get("type")
+            device_loc_id = request.args.get("locId")
+            smoke_id = request.args.get("smokeId")
+            gas_id = request.args.get("gasId")
+            temp_id = request.args.get("tempId")
+            return db.insert_device(device_name, device_type, device_loc_id, smoke_id, gas_id, temp_id)
+        except DatabaseError as e:
+            app.logger.exception(e)
+            return "Connection failed"
+
+
+@app.route('/sensor/smoke/new', methods=['POST'])
+def insert_smoke_sensor():
+    try:
+        smoke_name = request.args.get("smoke_name")
+        loc_id = request.args.get("loc_id")
+        return db.insert_smoke_sensor(smoke_name, loc_id)
+    except DatabaseError as e:
+        app.logger.exception(e)
+        return "Connection failed"
+
+
+@app.route('/sensor/loc/new', methods=['POST'])
+def insert_loc_sensor():
+    try:
+        loc_name = request.args.get("loc_name")
+        return db.insert_sensor_loc(loc_name)
+    except DatabaseError as e:
+        app.logger.exception(e)
+        return "Connection failed"
+
+
+@app.route('/sensor/gas/new', methods=['POST'])
+def insert_gas_sensor():
+    try:
+        gas_name = request.args.get("gas_name")
+        loc_id = request.args.get("loc_id")
+        return db.insert_gas_sensor(gas_name, loc_id)
+    except DatabaseError as e:
+        app.logger.exception(e)
+        return "Connection failed"
+
+
+@app.route('/sensor/temp/new', methods=['POST'])
+def insert_temp_sensor():
+    try:
+        temp_name = request.args.get("temp_name")
+        loc_id = request.args.get("loc_id")
+        return db.insert_temp_sensor(temp_name, loc_id)
+    except DatabaseError as e:
+        app.logger.exception(e)
+        return "Connection failed"
+
+
+@app.route('/sensor/history/new', methods=['POST'])
+def insert_history_sensor():
+    try:
+        device_id = request.args.get("device_id")
+        temp_reading = request.args.get("temp_reading")
+        smoke_reading = request.args.get("smoke_reading")
+        gas_reading = request.args.get("gas_reading")
+        date_reading = request.args.get("date_reading")
+        temp_id = request.args.get("temp_id")
+        smoke_id = request.args.get("smoke_id")
+        gas_id = request.args.get("gas_id")
+        return db.insert_history_sensor(device_id, temp_reading, smoke_reading, gas_reading, date_reading, temp_id,
+                                        smoke_id, gas_id)
+    except DatabaseError as e:
+        app.logger.exception(e)
+        return "Connection failed"
 
 
 if __name__ == '__ main__':
